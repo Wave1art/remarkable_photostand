f7717f94dc6242c50926aae3de33e7714e94aa68
Toby Monk <toby.monk@teamgoldflame.com>
2025-12-21 20:33:40 +0100

    Update service URL in install.sh script

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ unzip -o release.zip
 
 # install systemd service
 # mv renews.service ${SERVICE}
-wget -O ${SERVICE} "https://evidlo.github.io/remarkable_news/services/${1}.service"
+wget -O ${SERVICE} "https://wave1art.github.io/remarkable_news/services/${1}.service"
 
 # substitute COOLDOWN and KEYWORDS arguments
 if [[ -z $COOLDOWN ]]
